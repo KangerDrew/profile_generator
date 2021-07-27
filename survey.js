7440f86269e4dc12eccaa4ccd5b762fd66fffd06
Drew Kang <kangdrew95@gmail.com>
2021-07-27 22:34:07 +0000

    Finished the assignment, Fun Profile Generator

--- survey.js
+++ survey.js
@@ -5,8 +5,24 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+rl.question("What's your name? Nicknames are also acceptable :)  ", (answer1) => {
+  rl.question("What's an activity you like doing?  ", (answer2) => {
+    rl.question("What do you listen to while doing that?  ", (answer3) => {
+      rl.question("Which meal is your favourite (eg: dinner, brunch, etc.)?  ", (answer4) => {
+        rl.question("What's your favourite thing to eat for that meal?  ", (answer5) => {
+          rl.question("Which sport is your absolute favourite?  ", (answer6) => {
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at!  ", (answer7) => {
+              console.log("___________________________________");
+              console.log('***********YOUR RESPONSE***********');
+              console.log("___________________________________");
+              console.log(`${answer1} loves listening to ${answer3} while ${answer2}, devouring ${answer5}` +
+               `for ${answer4}, prefers ${answer6} over any other sport, and is amazing at ${answer7}.`);
 
-  rl.close();
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
 });
